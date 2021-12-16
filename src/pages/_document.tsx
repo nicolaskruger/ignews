ddd9894a5b71b44f281aa8fc5e90ff79d523c078
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-16 13:41:47 -0300

    feat: titulo dinamico por pagina

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,9 +6,7 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,900&display=swap" rel="stylesheet" />
-                    <title>IgNews</title>
                 </Head>
 
                 <body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { NextPage } from "next";
 import style from "../styles/home.module.scss";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <h1 className={style.title}>
-      Hello <span>Word</span>
-    </h1>
+    <>
+      <Head>
+        <title>Início | IgNews</title>
+      </Head>
+
+      <h1 className={style.title}>
+        Hello <span>Word</span>
+      </h1>
+    </>
   )
 }
 
