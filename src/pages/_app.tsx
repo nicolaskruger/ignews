d95682e6cebd7b468ad9876d946cb1bf1305ac52
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-16 16:18:07 -0300

    feat: subscribe button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import "../styles/global.scss";
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return <>
     <Header />
-    <Component {...pageProps} />;
+    <Component {...pageProps} />
   </>
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
 const Home: NextPage = () => {
@@ -17,6 +18,7 @@ const Home: NextPage = () => {
             get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
