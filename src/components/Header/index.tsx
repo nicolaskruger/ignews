1b427958a48f1402cc36388a74d367c488e8a613
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-22 16:04:10 -0300

    feat: navegacao menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { FC } from "react";
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export const Header: FC = () => {
+
+    const router = useRouter();
+
+    const validClassPath = (expected: string) => {
+        return router.pathname === expected ? styles.active : ""
+    }
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>home</a>
-                    <a>posts</a>
+                    <Link href={"/"}>
+                        <a className={validClassPath("/")}>home</a>
+                    </Link>
+                    <Link href={"/posts"} prefetch>
+                        <a className={validClassPath("/posts")}>
+                            posts
+                        </a>
+                    </Link>
                 </nav>
                 <SignInButton />
             </div>
