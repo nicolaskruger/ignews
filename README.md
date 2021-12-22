5d8e7afc6c6d007cacadbd8cffad5092670fbc1d
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-12-22 10:39:01 -0300

    feat: webhook stripe

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ignews
 
 [figma](<https://www.figma.com/file/9RMSmVJnKIsWadE6doPVP1/ig.news-(Copy)?node-id=1%3A2>)
+
+```bash
+
+# inicia stripe listener
+
+stripe listen --forward-to localhost:3000/api/webhooks
+
+```
---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+const webhooks = (req: NextApiRequest, res: NextApiResponse) => {
+    console.log("evento recebido")
+    res.status(200).json({ ok: true })
+}
+
+export default webhooks
